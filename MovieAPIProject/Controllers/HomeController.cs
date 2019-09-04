ca139856713eadc6b6bf21df228cefa75295162b
Nathaniel-Simpson-D <simpson.n.0308@gmail.com>
2019-09-04 11:38:39 -0400

    added search method

--- MovieAPIProject/Controllers/HomeController.cs
+++ MovieAPIProject/Controllers/HomeController.cs
@@ -27,6 +27,22 @@ namespace MovieAPIProject.Controllers
            
             return View();
         }
+        public IActionResult SearchResult(string query)
+        {
+            var splited = query.Split(' ');
+            string resultQuery = splited[0];
+            int i = 0;
+            foreach(var item in splited)
+            {
+                if (i != 0)
+                {
+                    resultQuery += "+";
+                    resultQuery += item;
+                }
+            }
+            var result = GetSearchByKeyWord(resultQuery, _configuation).Result;
+            return View(result);
+        }
         public IActionResult Details(int id)
         {
             var movie = GetMovieById(id, _configuation).Result;
@@ -35,14 +51,22 @@ namespace MovieAPIProject.Controllers
         public static HttpClient GetClient()
         {
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://api.themoviedb.org/3/movie/");
+            client.BaseAddress = new Uri("https://api.themoviedb.org/3/");
             return client;
         }
         public static async Task<Movie> GetMovieById(int id, IConfiguration configuration)
         {
             var client = GetClient();
             string apiKey = configuration.GetSection("AppConfiguration")["ApiKey"];
-            var response = await client.GetAsync($"{id}?api_key={apiKey}");
+            var response = await client.GetAsync($"movie/{id}?api_key={apiKey}");
+            var result = await response.Content.ReadAsAsync<Movie>();
+            return result;
+        }
+        public static async Task<Movie> GetSearchByKeyWord(string search,IConfiguration configuration)
+        {
+            var client = GetClient();
+            string apiKey = configuration.GetSection("AppConfiguration")["ApiKey"];
+            var response = await client.GetAsync($"Search/Movie?api_key={apiKey}&query={search}");
             var result = await response.Content.ReadAsAsync<Movie>();
             return result;
         }
